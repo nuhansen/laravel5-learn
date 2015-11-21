d58483b6f679c65ef6d4a911a85f9af2ce558169
artikow <artikow@gmail.com>
2015-11-21 09:01:19 +0700

    authentication sample

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,11 +10,20 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
-Route::resource("items","ItemController");
-Route::resource("item_logs","ItemLogController");
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::resource("items", "ItemController");
+    Route::resource("item_logs", "ItemLogController");
+});
